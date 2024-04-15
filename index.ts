3605257ba6aa7478da111c30f8552d39e35565a4
Syeda Imaan Zahra <zahrasyedaimaan@gmail.com>
2024-04-15 14:57:12 -0700

    created a command line ATM

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 20000;
+
+let pinCode = 121418;
+let pinAnswer = await inquirer.prompt({
+  name: "pin",
+  message: "enter your pin code",
+  type: "number",
+});
+if (pinAnswer.pin === pinCode) {
+  console.log("Correct pin code!!!!!");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "please select option",
+      type: "list",
+      choices: ["withdraw", "check balance"],
+    },
+  ]);
+
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+      { name: "amount", message: "enter your amount", type: "number" },
+    ]);
+    myBalance -= amountAns.amount;
+    console.log(`Your remaining balace: ${myBalance}`)
+  }
+  else if (operationAns.operation === "check balance")
+    {console.log(`Your balance is ${myBalance}`)}
+
+}
+else {console.log('Incorrect pin code')}
